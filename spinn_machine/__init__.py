aaa20248415dfafa32f0b24a373da47e68a2d9a4
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-07-02 20:16:43 +0100

    Added more details to the doc

--- spinn_machine/__init__.py
+++ spinn_machine/__init__.py
@@ -1,2 +1,3 @@
-""" A python abstraction of a SpiNNaker Machine
+""" A python abstraction of a SpiNNaker Machine.  The main functionality\
+    is provided by :py:class:`spinn_machine.machine.Machine`.
 """
