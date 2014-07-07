33bb6b836a75a05ce0f4b42f0904b9faa41b6550
Petrut Antoniu Bogdan <petrutantoniu8@gmail.com>
2014-07-07 12:01:40 +0100

    Updated tests for the chip module to comply with other modified modules

--- unittests/test_chip.py
+++ unittests/test_chip.py
@@ -1,24 +1,73 @@
 __author__ = 'Petrut'
 
 import unittest
-import spinn_machine.processor as proc
-import spinn_machine.link as link
-import spinn_machine.sdram as sdram
-
+from spinn_machine import processor as proc , link as link , sdram as sdram, router as router, chip as chip
+import spinn_machine.exceptions as exc
+from collections import OrderedDict
 class TestingChip(unittest.TestCase):
     def test_create_chip(self):
-        #self.assertEqual(True, False)
-        #new_chip = Chip(0,0,proc.Processor(0))
+        flops = 1000
+        (E, NE, N, W, SW, S) = range(6)
+
         processors = list()
         for i in range(18):
-            processors.append(proc.Processor(i))
+            processors.append(proc.Processor(i, flops))
 
         links = list()
-        for i in range(6):
-            links.append(link.Link())
+        links.append(link.Link(0,0,0,0,1,S,S))
 
         SDRAM = sdram.SDRAM(128)
+        x= 0
+        y= 1
 
+        links = list()
+
+        links.append(link.Link(0,0,0,1,1,N,N))
+        links.append(link.Link(0,1,1,1,0,S,S))
+        links.append(link.Link(1,1,2,0,0,E,E))
+        links.append(link.Link(1,0,3,0,1,W,W))
+        r = router.Router(links,False,100,1024)
+
+        ip = "192.162.240.253"
+
+        new_chip = chip.Chip(x,y,processors,r, SDRAM,ip)
+
+        self.assertEqual(new_chip.x,x)
+        self.assertEqual(new_chip.y,y)
+        self.assertEqual(new_chip.ip_address,ip)
+        self.assertEqual(new_chip.sdram,SDRAM)
+        self.assertEqual(new_chip.router,r)
+        for p in processors:
+            self.assertTrue(p in new_chip.processors)
+
+    def test_create_chip_with_duplicate_processors(self):
+        with self.assertRaises(exc.SpinnMachineAlreadyExistsException):
+            flops = 1000
+            (E, NE, N, W, SW, S) = range(6)
+
+            processors = list()
+            for i in range(18):
+                processors.append(proc.Processor(i, flops))
+            processors.append(proc.Processor(10,flops+1))
+
+            links = list()
+            links.append(link.Link(0,0,0,0,1,S,S))
+
+            SDRAM = sdram.SDRAM(128)
+            x= 0
+            y= 1
+
+            links = list()
+
+            links.append(link.Link(0,0,0,1,1,N,N))
+            links.append(link.Link(0,1,1,1,0,S,S))
+            links.append(link.Link(1,1,2,0,0,E,E))
+            links.append(link.Link(1,0,3,0,1,W,W))
+            r = router.Router(links,False,100,1024)
+
+            ip = "192.162.240.253"
+
+            new_chip = chip.Chip(x,y,processors,r, SDRAM,ip)
 
 
 if __name__ == '__main__':
