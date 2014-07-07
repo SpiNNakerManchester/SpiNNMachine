ec0cb070d51cb02384431b0b7152a8276d5746b6
Petrut Antoniu Bogdan <petrutantoniu8@gmail.com>
2014-07-07 10:41:32 +0100

    Updated tests to match changes in the processor module

--- unittests/test_processor.py
+++ unittests/test_processor.py
@@ -7,12 +7,29 @@ class TestingProcessor(unittest.TestCase):
     def test_creating_processors(self):
         #self.assertEqual(True, False)
         processors = list()
+        flops = 1000
         for i in range(18):
-            processors.append(proc.Processor(i))
+            processors.append(proc.Processor(i, flops))
 
         for id in range(18):
-            self.assertEqual(processors[id]._processor_id, id)
+            self.assertEqual(processors[id].processor_id, id)
+            self.assertEqual(processors[id].clock_speed, flops)
+            self.assertFalse(processors[id].is_monitor)
 
+    def test_creating_monitor_processors(self):
+        processors = list()
+        flops = 1000
+        for i in range(18):
+            processors.append(proc.Processor(i, flops,is_monitor = True))
+
+        for id in range(18):
+            self.assertEqual(processors[id].processor_id, id)
+            self.assertEqual(processors[id].clock_speed, flops)
+            self.assertTrue(processors[id].is_monitor)
+
+    def test_creating_processors_with_negative_clock_speed(self):
+        with self.assertRaises(Exception):
+            proc.Processor(processor_id= 1, clock_speed= - 5)
 
 
 if __name__ == '__main__':
