2d41e3b1cd0a05f5491c90171b117107e96ec9d2
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-07-14 12:30:19 +0100

    Adding setup and requirements files

---
+++ setup.py
@@ -0,0 +1,12 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="SpiNNMachine",
+    version="0.1-SNAPSHOT",
+    description="Representation of a SpiNNaker Machine",
+    url="https://github.com/SpiNNakerManchester/SpiNNMachine",
+    packages=['spinnman'],
+)
