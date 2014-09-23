c7e2f8e8338c88b1d2793fa0bd3071add82237c6
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-09-23 14:56:04 +0100

    Created a readme, and added a license to the setup.py for pypi

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     version="0.1-SNAPSHOT",
     description="Representation of a SpiNNaker Machine",
     url="https://github.com/SpiNNakerManchester/SpiNNMachine",
+    license="GNU GPLv3.0",
     packages=['spinn_machine']
 )
