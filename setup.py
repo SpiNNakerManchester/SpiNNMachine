0c3dce44f403ea012c5ffbfcf4fd7bcc564ee3c0
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-01-07 08:49:00 +0000

    Updating version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="SpiNNMachine",
-    version="2014.12-rc-01",
+    version="2015.001",
     description="Representation of a SpiNNaker Machine",
     url="https://github.com/SpiNNakerManchester/SpiNNMachine",
     license="GNU GPLv3.0",
