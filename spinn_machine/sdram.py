6ad8545cc3a5515a4f09d1cf709f3d6ee152e86e
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-06-12 16:56:24 +0100

    Revert "Added some new properties to the SDRAM"
    This reverts commit 7a972fc3a9022a2acc4338199e0f6b5631a67f50.

--- spinn_machine/sdram.py
+++ spinn_machine/sdram.py
@@ -6,48 +6,21 @@ class SDRAM(object):
     """
     DEFAULT_SDRAM_BYTES = 128 * 1024 * 1024
 
-    def __init__(self, user_base_address, system_base_address,
-                 total_size=DEFAULT_SDRAM_BYTES):
+    def __init__(self, size=DEFAULT_SDRAM_BYTES):
         """
 
         :param size: size of the SDRAM in bytes
         :type size: int
-        :param user_base_address: The start of the space which is available\
-                    for use
-        :type user_base_address: int
-        :param system_base_address: The start of the space which is reserved\
-                    for use by the system
-        :type user_base_address: int
         :raise spinn_machine.exceptions.SpinnMachineInvalidParameterException:\
                     If the size is less than 0
         """
-        if total_size < 0:
+        if size < 0:
             raise SpinnMachineInvalidParameterException(
-                "size", str(total_size), "Must not be less than 0")
-        self._total_size = total_size
-        self._user_base_address = user_base_address
-        self._system_base_address = system_base_address
+                "size", str(size), "Must not be less than 0")
+        self._size = size
 
     @property
     def size(self):
-        """ The SDRAM available for user applications
-
-        :return: The space available in bytes
-        :rtype: int
-        """
-        return self._system_base_address - self._user_base_addr
-
-    @property
-    def user_base_address(self):
-        """ The start of the SDRAM available for user applications
-
-        :return: The start of the user space in SDRAM
-        :rtype: int
-        """
-        return self._user_base_address
-
-    @property
-    def total_size(self):
         """ The size of SDRAM in bytes
 
         :return: The size of SDRAM in bytes
@@ -55,9 +28,9 @@ class SDRAM(object):
         :raise None: does not raise any known exceptions
         """
         return self._size
-
+    
     def __str__(self):
         return "{} MB".format(self._size / (1024 * 1024))
-
+    
     def __repr__(self):
         return self.__str__()
