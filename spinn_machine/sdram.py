27bb72b70a3a6ecdee36c8dae6b6b7c1e1373a64
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-05-12 11:30:35 +0100

    Fixed the SDRAM object to make more sense in the context of scamp-3

--- spinn_machine/sdram.py
+++ spinn_machine/sdram.py
@@ -11,17 +11,16 @@ class SDRAM(object):
     DEFAULT_SYSTEM_ADDRESS = 0x77800000
 
     def __init__(self, user_base_address=DEFAULT_BASE_ADDRESS,
-                 system_base_address=DEFAULT_SYSTEM_ADDRESS,
-                 total_size=DEFAULT_SDRAM_BYTES):
+                 total_size=DEFAULT_SDRAM_BYTES,
+                 free_size=DEFAULT_SDRAM_BYTES):
         """
         :param user_base_address: The start of the space which is available\
                     for use
         :type user_base_address: int
-        :param system_base_address: The start of the space which is reserved\
-                    for use by the system
         :type user_base_address: int
         :param total_size: the total size of bytes that this sdram object holds
         :type total_size: int
+        :param free_size: the space available in SDRAM
         :raise spinn_machine.exceptions.SpinnMachineInvalidParameterException:\
                     If the size is less than 0
         """
@@ -29,8 +28,8 @@ class SDRAM(object):
             raise SpinnMachineInvalidParameterException(
                 "size", str(total_size), "Must not be less than 0")
         self._user_base_address = user_base_address
-        self._system_base_address = system_base_address
         self._total_size = total_size
+        self._free_size = free_size
 
     @property
     def size(self):
@@ -39,7 +38,7 @@ class SDRAM(object):
         :return: The space available in bytes
         :rtype: int
         """
-        return self._system_base_address - self._user_base_address
+        return self._free_size
 
     @property
     def user_base_address(self):
@@ -61,7 +60,7 @@ class SDRAM(object):
         return self._total_size
 
     def __str__(self):
-        return "{} MB".format(self._total_size / (1024 * 1024))
+        return "{} MB".format(self._free_size / (1024 * 1024))
 
     def __repr__(self):
         return self.__str__()
