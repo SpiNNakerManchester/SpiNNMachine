0f2ba0425a87728bc3d357a3ece87470c09b3ed1
alan-stokes <alan.barry.stokes@gmail.com>
2016-07-28 11:33:28 -0400

    fixed spinnmachine

--- spinn_machine/resources/ip_tag_resource.py
+++
@@ -1,9 +0,0 @@
-from spinn_machine.resources.abstract_resource import AbstractResource
-
-
-class IPTagResource(AbstractResource):
-    """ An IP tag resource that can be either a forward or reverse tag
-    """
-
-    __slots__ = [
-    ]
