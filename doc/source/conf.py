1baf2e5ae4cf9659c0a70b6b47ad462f7a60d86c
Christian <brenninc@cs.man.ac.uk>
2017-02-13 11:26:26 +0000

    Added links to doc homepage and update copywrite

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SpiNNMachine'
-copyright = u'2014'
+copyright = u'2014-2017'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -1,3 +1,10 @@
+These pages document the python code for the SpiNNMachine_ module
+which is part of the SpiNNaker_ Project.
+
+.. _SpiNNMachine: https://github.com/SpiNNakerManchester/SpiNNMachine
+.. _SpiNNaker: http://apt.cs.manchester.ac.uk/projects/SpiNNaker/
+
+
 SpiNNMachine
 ============
 
