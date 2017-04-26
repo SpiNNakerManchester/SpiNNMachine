a84dd18246acd303ab3d498d08aa2cf5e47f436f
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-04-26 08:54:33 +0100

    Updated versions

--- setup.py
+++ setup.py
@@ -28,13 +28,13 @@ for dirname, dirnames, filenames in os.walk(main_package_dir):
 
 setup(
     name="SpiNNMachine",
-    version="3.0.0",
+    version="1!4.0.0a1",
     description="Representation of a SpiNNaker Machine",
     url="https://github.com/SpiNNakerManchester/SpiNNMachine",
     license="GNU GPLv3.0",
     packages=packages,
     package_data=package_data,
-    install_requires=['SpiNNUtilities >= 3.0.0, < 4.0.0',
+    install_requires=['SpiNNUtilities >= 4.0.0a1, < 5.0.0',
                       'six']
 
 )
