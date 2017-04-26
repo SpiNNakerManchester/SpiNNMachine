3c76f78b773cb320f5dc3e0213ce27d0a147fd3b
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-04-26 09:02:44 +0100

    Updated again to include epoch

--- setup.py
+++ setup.py
@@ -28,13 +28,13 @@ for dirname, dirnames, filenames in os.walk(main_package_dir):
 
 setup(
     name="SpiNNMachine",
-    version="1!4.0.0a1",
+    version="1!4.0.0a2",
     description="Representation of a SpiNNaker Machine",
     url="https://github.com/SpiNNakerManchester/SpiNNMachine",
     license="GNU GPLv3.0",
     packages=packages,
     package_data=package_data,
-    install_requires=['SpiNNUtilities >= 4.0.0a1, < 5.0.0',
+    install_requires=['SpiNNUtilities >= 1!4.0.0a1, < 1!5.0.0',
                       'six']
 
 )
