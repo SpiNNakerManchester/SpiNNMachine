9ece79bca242cf064b3de31c4096d99a45140a41
Christian <brenninc@cs.man.ac.uk>
2017-05-03 14:13:24 +0100

    fixed version test

--- unittests/test_version.py
+++ unittests/test_version.py
@@ -1,15 +1,14 @@
 import spinn_utilities
-import spinn_storage_handlers
+import spinn_machine
 
 
-def test_import_all():
+def test_compare_versions():
     spinn_utilities_parts = spinn_utilities.__version__.split('.')
-    spinn_storage_handlers_parts = spinn_storage_handlers.\
-        __version__.split('.')
+    spinn_machine_parts = spinn_machine.__version__.split('.')
 
-    assert (spinn_utilities_parts[0] == spinn_storage_handlers_parts[0])
-    assert (spinn_utilities_parts[1] <= spinn_storage_handlers_parts[1])
+    assert (spinn_utilities_parts[0] == spinn_machine_parts[0])
+    assert (spinn_utilities_parts[1] <= spinn_machine_parts[1])
 
 
 if __name__ == '__main__':
-    test_import_all()
+    test_compare_versions()
