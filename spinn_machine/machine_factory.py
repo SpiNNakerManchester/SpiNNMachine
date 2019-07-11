dd9061ef912e6f377e356f51e13c1531aef46e1d
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2019-07-11 13:45:57 +0100

    First cut at satisfying the RAT

--- spinn_machine/machine_factory.py
+++ spinn_machine/machine_factory.py
@@ -1,13 +1,28 @@
+# Copyright (c) 2019 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import logging
-from .no_wrap_machine import NoWrapMachine
-from .horizontal_wrap_machine import HorizontalWrapMachine
-from .vertical_wrap_machine import VerticalWrapMachine
-from .full_wrap_machine import FullWrapMachine
 try:
     from collections.abc import defaultdict
 except ImportError:
     from collections import defaultdict
 from spinn_machine import (Chip, Router)
+from .no_wrap_machine import NoWrapMachine
+from .horizontal_wrap_machine import HorizontalWrapMachine
+from .vertical_wrap_machine import VerticalWrapMachine
+from .full_wrap_machine import FullWrapMachine
 from .exceptions import SpinnMachineException
 
 logger = logging.getLogger(__name__)
