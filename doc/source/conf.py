f56b705c0d8638884beb1c7d665548e5aa3bddb7
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-05-10 17:06:49 +0100

    sphinx magic copied from FEC

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -30,6 +30,8 @@
 
 # import sys
 import os
+import re
+import sys
 from sphinx.ext import apidoc
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -359,6 +361,29 @@ epub_exclude_files = ['search.html']
 
 autoclass_content = 'both'
 
+_package_base = "spinn_machine"
+
+
+# Automatically called by sphinx at startup
+def setup(app):
+    # NB: extra dot at end is deliberate!
+    trim = (_package_base + ".", "spinn_utilities.")
+
+    # Magic to shorten the names of our classes to their public versions
+    def skip_handler(_app, what, name, obj, skip, _options):
+        if not skip and what == 'module' and hasattr(obj, "__module__"):
+            # Get parent module *and* check if our name is in it
+            m = re.sub(r'\.[a-z0-9_]+$', '', obj.__module__)
+            if any(m.startswith(prefix) for prefix in trim) and \
+                    name in dir(sys.modules[m]):
+                # It is, so update to say that's canonical location for
+                # documentation purposes
+                obj.__module__ = m
+        return skip  # We don't care to change this
+
+    # Connect the callback to the autodoc-skip-member event from apidoc
+    app.connect('autodoc-skip-member', skip_handler)
+
 
 def filtered_files(base, unfiltered_files_filename):
     with open(unfiltered_files_filename) as f:
@@ -377,7 +402,6 @@ def filtered_files(base, unfiltered_files_filename):
 
 _output_dir = os.path.abspath(".")
 _unfiltered_files = os.path.abspath("../unfiltered-files.txt")
-_package_base = "spinn_machine"
 
 # Do the rst generation; remove files which aren't in git first!
 for fl in os.listdir("."):
