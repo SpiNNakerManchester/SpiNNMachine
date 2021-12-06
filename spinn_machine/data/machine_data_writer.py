ddf754e5094cfccfda84bd0100cae7c8c751dbab
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-12-06 17:01:19 +0000

    machine in global data

---
+++ spinn_machine/data/machine_data_writer.py
@@ -0,0 +1,72 @@
+# Copyright (c) 2017-2019 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+from spinn_utilities.data.utils_data_writer import UtilsDataWriter
+from spinn_utilities.log import FormatAdapter
+from spinn_machine import Machine
+from .machine_data_view import MachineDataView, _MachineDataModel
+
+logger = FormatAdapter(logging.getLogger(__name__))
+__temp_dir = None
+
+REPORTS_DIRNAME = "reports"
+
+
+class MachineDataWriter(UtilsDataWriter, MachineDataView):
+    """
+    Writer class for the Fec Data
+
+    """
+    __data = _MachineDataModel()
+    __slots__ = []
+
+    def mock(self):
+        """
+        Clears out all data and adds mock values where needed.
+
+        This should set the most likely defaults values.
+        But be aware that what is considered the most likely default could
+        change over time.
+
+        Unittests that depend on any valid value being set should be able to
+        depend on Mock.
+
+        Unittest that depend on a specific value should call mock and then
+        set that value.
+        """
+        UtilsDataWriter.mock(self)
+        self.__data._clear()
+
+    def setup(self):
+        """
+        Puts all data back into the state expected at sim.setup time
+
+        """
+        UtilsDataWriter.setup(self)
+        self.__data._clear()
+
+    def hard_reset(self):
+        UtilsDataWriter.hard_reset(self)
+        self.__data._hard_reset()
+
+    def soft_reset(self):
+        UtilsDataWriter.soft_reset(self)
+        self.__data._soft_reset()
+
+    def set_machine(self, machine):
+        if not isinstance(machine, Machine):
+            raise TypeError("machine should be a Machine")
+        self.__data._machine = machine
---
+++ unittests/data/test_data.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2021 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+from spinn_utilities.exceptions import (DataNotYetAvialable)
+from spinn_machine import virtual_machine
+from spinn_machine.config_setup import unittest_setup
+from spinn_machine.data import MachineDataView
+from spinn_machine.data.machine_data_writer import MachineDataWriter
+
+
+class TestSimulatorData(unittest.TestCase):
+
+    def setUp(cls):
+        unittest_setup()
+
+    def test_setup(self):
+        view = MachineDataView()
+        writer = MachineDataWriter()
+        # What happens before setup depends on the previous test
+        # Use manual_check to verify this without dependency
+        writer.setup()
+        with self.assertRaises(DataNotYetAvialable):
+            view.machine
+
+    def test_mock(self):
+        view = MachineDataView()
+        writer = MachineDataWriter()
+        writer.mock()
+        # check there is a
+        #   value not what it is
+        view.machine
+
+    def test_machine(self):
+        view = MachineDataView()
+        writer = MachineDataWriter()
+        writer.setup()
+        with self.assertRaises(DataNotYetAvialable):
+            view.machine
+        writer.set_machine(virtual_machine(width=2, height=2))
+        self.assertEqual(4, view.machine.n_chips)
+        with self.assertRaises(TypeError):
+            writer.set_machine("bacon")
