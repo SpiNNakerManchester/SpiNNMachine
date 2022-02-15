5b22f11374ef0e225577d128cbec69629605fd1c
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2022-02-15 09:21:29 +0000

    Make it real

--- spinn_machine/ignores/ignore_core.py
+++ spinn_machine/ignores/ignore_core.py
@@ -18,7 +18,7 @@ TYPICAL_PHYSICAL_VIRTUAL_MAP = {
     0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8, 8: 9, 9: 10, 10: 0, 11: 12,
     12: 13, 13: 14, 14: 15, 15: 16, 16: 17, 17: 18}
 
-CORE_RANGE = re.compile("(\d+)-(\d+)")
+CORE_RANGE = re.compile(r"(\d+)-(\d+)")
 
 
 class IgnoreCore(object):
