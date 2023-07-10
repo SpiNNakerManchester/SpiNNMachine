477657b0cdbe612ab8dc2a037b2a7a27d687c6c3
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2023-07-10 13:58:27 +0100

    setUp is not a class method

--- unittests/data/test_data.py
+++ unittests/data/test_data.py
@@ -22,7 +22,7 @@ from spinn_machine.data.machine_data_writer import MachineDataWriter
 
 class TestSimulatorData(unittest.TestCase):
 
-    def setUp(cls):
+    def setUp(self):
         unittest_setup()
 
     def test_setup(self):
