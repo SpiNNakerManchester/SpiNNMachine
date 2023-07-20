fbdeeaae21b85ceb49d22101833927d5be698003
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-07-20 15:54:37 +0100

    Version class tests

--- unittests/version/test_version3.py
+++ unittests/version/test_version3.py
@@ -21,89 +21,88 @@ from spinn_machine.full_wrap_machine import FullWrapMachine
 from spinn_machine.no_wrap_machine import NoWrapMachine
 from spinn_machine.horizontal_wrap_machine import HorizontalWrapMachine
 from spinn_machine.vertical_wrap_machine import VerticalWrapMachine
-from spinn_machine.version.version_5 import Version5
+from spinn_machine.version.version_3 import Version3
 from spinn_machine.config_setup import unittest_setup
 from spinn_machine.exceptions import SpinnMachineException
 
 
-class TestVersion5(unittest.TestCase):
+class TestVersion3(unittest.TestCase):
     """ Tests of IPTag
     """
     def setUp(self):
         unittest_setup()
 
     def test_attributes(self):
-        version = Version5()
+        version = Version3()
         self.assertEqual(18, version.max_cores_per_chip)
         self.assertEqual(123469792, version.max_sdram_per_chip)
         self.assertEqual(1, version.n_non_user_cores)
-        self.assertEqual("Spin1 48 Chip", version.name)
-        self.assertEqual(48, version.n_chips_per_board)
+        self.assertEqual("Spin1 4 Chip", version.name)
+        self.assertEqual(4, version.n_chips_per_board)
         self.assertEqual(1023, version.n_router_entries)
 
     def test_verify_config_width_height(self):
         set_config("Machine", "width", "None")
         set_config("Machine", "height", "None")
-        Version5()
+        Version3()
 
-        set_config("Machine", "width", 8)
+        set_config("Machine", "width", 2)
         with self.assertRaises(SpinnMachineException):
-            Version5()
+            Version3()
 
-        set_config("Machine", "height", 8)
-        Version5()
+        set_config("Machine", "height", 2)
+        Version3()
 
         set_config("Machine", "width", "None")
         with self.assertRaises(SpinnMachineException):
-            Version5()
+            Version3()
 
     def test_set_max_lower(self):
         set_config("Machine", "max_sdram_allowed_per_chip", 1000)
         set_config("Machine", "max_machine_core", 10)
-        version = Version5()
+        version = Version3()
         self.assertEqual(10, version.max_cores_per_chip)
         self.assertEqual(1000, version.max_sdram_per_chip)
 
     def test_expected_xys(self):
-        version = Version5()
+        version = Version3()
         xys = version.expected_xys
-        self.assertEqual(48, len(xys))
-        self.assertEqual(48, len(set(xys)))
+        self.assertEqual(4, len(xys))
+        self.assertEqual(4, len(set(xys)))
         for (x, y) in xys:
             self.assertGreaterEqual(x, 0)
             self.assertGreaterEqual(y, 0)
-            self.assertLess(x, 8)
-            self.assertLess(y, 8)
+            self.assertLess(x, 2)
+            self.assertLess(y, 2)
 
     def test_expected_chip_core_map(self):
-        version = Version5()
+        version = Version3()
         chip_core_map = version.chip_core_map
-        self.assertEqual(48, len(chip_core_map))
+        self.assertEqual(4, len(chip_core_map))
         for (x, y) in chip_core_map:
             self.assertGreaterEqual(x, 0)
             self.assertGreaterEqual(y, 0)
-            self.assertLess(x, 8)
-            self.assertLess(y, 8)
+            self.assertLess(x, 2)
+            self.assertLess(y, 2)
             cores = chip_core_map[(x, y)]
             self.assertGreaterEqual(cores, 16)
             self.assertLessEqual(cores, 18)
 
     def test_get_potential_ethernet_chips(self):
-        version = Version5()
-        eths = version.get_potential_ethernet_chips(8, 8)
-        self.assertListEqual([(0, 0)], eths)
-        eths = version.get_potential_ethernet_chips(12, 12)
-        self.assertListEqual([(0, 0), (4, 8), (8, 4)], eths)
-        eths = version.get_potential_ethernet_chips(16, 16)
-        self.assertListEqual([(0, 0), (4, 8), (8, 4)], eths)
-
-        # if size is wrong GIGO
+        version = Version3()
         eths = version.get_potential_ethernet_chips(2, 2)
         self.assertListEqual([(0, 0)], eths)
 
+        # if size is wromg GIGO
+        eths = version.get_potential_ethernet_chips(8, 8)
+        self.assertListEqual([(0, 0)], eths)
+        eths = version.get_potential_ethernet_chips(12, 12)
+        self.assertListEqual([(0, 0)], eths)
+        eths = version.get_potential_ethernet_chips(16, 16)
+        self.assertListEqual([(0, 0)], eths)
 
     def test_verify_size(self):
-        version = Version5()
+        version = Version3()
 
         with self.assertRaises(SpinnMachineException):
             version.verify_size(12, -12)
@@ -117,32 +116,26 @@ class TestVersion5(unittest.TestCase):
             version.verify_size(12, 8)
         with self.assertRaises(SpinnMachineException):
             version.verify_size(8, 12)
+        version.verify_size(2, 2)
         with self.assertRaises(SpinnMachineException):
-            version.verify_size(2, 2)
-
-        version.verify_size(8, 8)
+            version.verify_size(8, 8)
         with self.assertRaises(SpinnMachineException):
             version.verify_size(12, 8)
-        version.verify_size(12, 12)
-        version.verify_size(12, 16)
-        version.verify_size(16, 12)
-        version.verify_size(16, 16)
+        with self.assertRaises(SpinnMachineException):
+            version.verify_size(12, 12)
+        with self.assertRaises(SpinnMachineException):
+            version.verify_size(12, 16)
+        with self.assertRaises(SpinnMachineException):
+            version.verify_size(16, 12)
+        with self.assertRaises(SpinnMachineException):
+            version.verify_size(16, 16)
 
     def test_create_machin(self):
-        version = Version5()
+        version = Version3()
 
-        machine = version.create_machine(width=8, height=8)
-        self.assertIsInstance(machine,  NoWrapMachine)
-
-        machine = version.create_machine(16, 16)
-        self.assertIsInstance(machine,  NoWrapMachine)
-
-        machine = version.create_machine(12, 16)
-        self.assertIsInstance(machine, HorizontalWrapMachine)
-        machine = version.create_machine(16, 12)
-        self.assertIsInstance(machine, VerticalWrapMachine)
-        machine = version.create_machine(12, 12)
+        machine = version.create_machine(width=2, height=2)
         self.assertIsInstance(machine, FullWrapMachine)
+
 
 if __name__ == '__main__':
     unittest.main()
