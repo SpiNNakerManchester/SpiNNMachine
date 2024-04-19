07e1c6baf5a1b9d90b0532400e8328e9c5bd1e44
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-04-19 11:38:33 +0100

    fix merge errors

--- unittests/test_machine.py
+++ unittests/test_machine.py
@@ -54,7 +54,7 @@ class SpinnMachineTestCase(unittest.TestCase):
     def _create_chip(self, x, y):
         n_cores = MachineDataView.get_machine_version().max_cores_per_chip
         if x == y == 0:
-            return Chip(x, y, [0], range(1, n_cores, self._router, self._sdram,
+            return Chip(x, y, [0], range(1, n_cores), self._router, self._sdram,
                         self._nearest_ethernet_chip[0],
                         self._nearest_ethernet_chip[1], self._ip)
         return Chip(x, y, [0], range(1, n_cores), self._router, self._sdram,
