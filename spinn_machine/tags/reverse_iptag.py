9f20dc8b067347c48b949452d7dfe1efb63bb1df
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-11-13 08:34:36 +0000

    remove unused import

--- spinn_machine/tags/reverse_iptag.py
+++ spinn_machine/tags/reverse_iptag.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing_extensions import Optional
 from .abstract_tag import AbstractTag
 
 
