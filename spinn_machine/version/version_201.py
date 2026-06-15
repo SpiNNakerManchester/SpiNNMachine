2e4ae6d63ee55c10de74c3b19c2c54b5ca789a80
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2026-06-15 08:22:48 +0100

    remove unused imports

--- spinn_machine/version/version_201.py
+++ spinn_machine/version/version_201.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Dict, Final, List, Optional, Sequence, Tuple
+from typing import Any, Dict, Final, Optional, Sequence, Tuple
 
 from spinn_utilities.typing.coords import XY
 from spinn_utilities.overrides import overrides
