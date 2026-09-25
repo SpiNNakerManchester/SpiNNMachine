3c19d0eced8c519af512d07c69aaa899f12ecd7c
Christian-B <christian.brenninkmeijer@manchester.ac.uk>
2026-09-25 11:17:28 +0100

    fix dict reverse

--- spinn_machine/version/version_spin2.py
+++ spinn_machine/version/version_spin2.py
@@ -79,7 +79,8 @@ class VersionSpin2(AbstractVersion, metaclass=AbstractBase):
         super().__init__(max_cores_per_chip=153,
                          max_sdram_per_chip=1073741824)
         self._reverse_quad_map: dict[tuple[int, int, int], int] = {
-            (v, k) for k, v in QUAD_MAP.items()}
+            v: k for k, v in QUAD_MAP.items()}
+        a = 1
 
     @property
     @overrides(AbstractVersion.n_scamp_cores)
